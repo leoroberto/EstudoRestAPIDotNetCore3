f9e0a60c1e61f333f9023a33399b0ae735996615
Leonardo Farias <leoroberto@gmail.com>
2020-05-03 20:09:11 -0300

    Inclusão dos arquivos referentes aos estudos dos controllers da API, enquanto não obtenho reposta sobre o problema

--- src/DevIO.API/Controllers/FornecedoresController.cs
+++ src/DevIO.API/Controllers/FornecedoresController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -8,22 +9,39 @@ using Microsoft.AspNetCore.Mvc;
 namespace DevIO.API.Controllers
 {
     [Route("api/fornecedores")]
-    public abstract class FornecedoresController : MainController
+    public class FornecedoresController : MainController
     {
         private readonly IFornecedorRepository _fornecedorRepository;
         private readonly IMapper _mapper;
 
-        protected FornecedoresController(IFornecedorRepository fornecedorRepository, IMapper mapper)
+        public FornecedoresController(IFornecedorRepository fornecedorRepository, IMapper mapper)
         {
             _fornecedorRepository = fornecedorRepository;
             _mapper = mapper;
         }
         
+        [Http]
         public async Task<IEnumerable<FornecedorViewModel>> ObterTodos()
         {
             var fornecedor = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
             
             return fornecedor;
         }
+        
+        public async Task<ActionResult<FornecedorViewModel>> ObterPorId(Guid id)
+        {
+            var fornecedor = await ObterFornecedorProdutosEndereco(id);
+
+            if (fornecedor == null) return NotFound();
+            
+            return fornecedor;
+        }
+
+        public async Task<FornecedorViewModel> ObterFornecedorProdutosEndereco(Guid id)
+        {
+            return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorProdutosEndereco(id));
+        }
+        
+        
     }
 }
